e376ab9ecfa86e01f75383daec3977b125528f77
Anna Krystalli <annakrystalli@googlemail.com>
2023-07-25 17:17:11 +0300

    add check_file_name check

---
+++ tests/testthat/test-check_file_name.R
@@ -0,0 +1,30 @@
+test_that("check_file_name works when file name valid", {
+    expect_snapshot(
+        check_file_name(
+            "model-output/team1-goodmodel/2022-10-08-team1-goodmodel.csv"
+        )
+    )
+    expect_snapshot(
+        check_file_name(
+            "model-output/team1-goodmodel/2022-10-08-team1-good_model.csv"
+        )
+    )
+    expect_snapshot(
+        check_file_name(
+            "model-output/team1-goodmodel/round_1-team1-goodmodel.parquet"
+        )
+    )
+})
+
+test_that("check_file_name works when file name fails", {
+    expect_snapshot(
+        check_file_name(
+            "model-output/team1-goodmodel/2022-10-08-team1_goodmodel.csv"
+        )
+    )
+    expect_snapshot(
+        check_file_name(
+            "model-output/team1-goodmodel/round_1-team1-good-model.parquet"
+        )
+    )
+})
