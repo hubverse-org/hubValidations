6eae17665df790a31587d5ec1edd847dca9a5584
Anna Krystalli <annakrystalli@googlemail.com>
2023-08-11 11:01:37 +0300

    optimise conc rows (complex)

--- R/utils.R
+++ R/utils.R
@@ -37,36 +37,63 @@ rel_file_path <- function(file_path, hub_path) {
 }
 
 get_file_round_id <- function(file_path) {
-    parse_file_name(file_path)$round_id
+  parse_file_name(file_path)$round_id
 }
 
 
 get_file_round_idx <- function(file_path, hub_path) {
-    round_id <- get_file_round_id(file_path)
-    config_tasks <- hubUtils::read_config(hub_path, "tasks")
-    hubUtils::get_round_idx(config_tasks, round_id)
+  round_id <- get_file_round_id(file_path)
+  config_tasks <- hubUtils::read_config(hub_path, "tasks")
+  hubUtils::get_round_idx(config_tasks, round_id)
 }
 
 get_file_round_config <- function(file_path, hub_path) {
-    config_tasks <- hubUtils::read_config(hub_path, "tasks")
-    round_idx <- get_file_round_idx(file_path, hub_path)
-    config_tasks[["rounds"]][[round_idx]]
+  config_tasks <- hubUtils::read_config(hub_path, "tasks")
+  round_idx <- get_file_round_idx(file_path, hub_path)
+  config_tasks[["rounds"]][[round_idx]]
 }
 
 is_round_id_from_variable <- function(file_path, hub_path) {
-    get_file_round_config(file_path, hub_path)[["round_id_from_variable"]]
+  get_file_round_config(file_path, hub_path)[["round_id_from_variable"]]
 }
 
 
 conc_rows <- function(tbl, mask = NULL, sep = "-") {
   if (is.null(mask)) {
-    apply(tbl, 1, function(x){paste(x, collapse = sep)})
+    tbl_dim <- dim(tbl)
+    rows <- split(
+      unlist(tbl, use.names = FALSE),
+      rep(1:tbl_dim[1], tbl_dim[2])
+    )
+    out <- lapply(rows, function(x) {
+      paste(x, collapse = sep)
+    }) %>%
+      unlist(use.names = FALSE)
+    return(out)
   } else {
-    purrr::map2_chr(
-      split(tbl, f = seq_along(tbl[[1]])),
-      split(mask, f = seq_along(tbl[[1]])),
-    ~ paste(.x[, !.y], collapse = sep)) %>%
-      unname()
+    vec <- as.matrix(tbl)[!mask]
+    if (all(rowSums(mask) == ncol(mask) - 1L)) {
+      return(vec)
+    }
+    if (any(rowSums(mask) == ncol(mask))) {
+      indx <- which(!mask, arr.ind = TRUE)[, "row"]
+      empty_indx <- setdiff(1:nrow(tbl), indx)
+      empty_out <- rep("", length(empty_indx))
+
+      rows <- split(vec, indx)
+      out <- lapply(rows, function(x) {
+        paste(x, collapse = sep)
+      }) %>%
+        unlist(use.names = FALSE)
+      return(
+        c(out, empty_out)[order(c(as.integer(names(rows)), empty_indx))]
+      )
+    }
+    indx <- which(!mask, arr.ind = TRUE)[, "row"]
+    rows <- split(vec, indx)
+    lapply(rows, function(x) {
+      paste(x, collapse = sep)
+    }) %>%
+      unlist(use.names = FALSE)
   }
 }
-
