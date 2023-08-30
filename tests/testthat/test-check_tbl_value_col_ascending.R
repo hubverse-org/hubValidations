9ee594c1c356290fc8034ed1bb3b6b61ef311a12
Anna Krystalli <annakrystalli@googlemail.com>
2023-08-30 11:43:12 +0300

    remove unnecessary round ID specification

--- tests/testthat/test-check_tbl_value_col_ascending.R
+++ tests/testthat/test-check_tbl_value_col_ascending.R
@@ -2,7 +2,6 @@ test_that("check_tbl_value_col_ascending works", {
   hub_path <- system.file("testhubs/simple", package = "hubValidations")
   file_path <- "team1-goodmodel/2022-10-08-team1-goodmodel.csv"
   tbl <- hubValidations::read_model_out_file(file_path, hub_path)
-  round_id <- "2022-10-08"
 
   expect_snapshot(
     check_tbl_value_col_ascending(tbl, file_path)
@@ -10,7 +9,6 @@ test_that("check_tbl_value_col_ascending works", {
 
   hub_path <- system.file("testhubs/flusight", package = "hubUtils")
   file_path <- "hub-ensemble/2023-05-08-hub-ensemble.parquet"
-  round_id <- "2023-05-08"
   tbl <- hubValidations::read_model_out_file(file_path, hub_path)
 
   expect_snapshot(
@@ -23,7 +21,6 @@ test_that("check_tbl_value_col_ascending errors correctly", {
   hub_path <- system.file("testhubs/simple", package = "hubValidations")
   file_path <- "team1-goodmodel/2022-10-08-team1-goodmodel.csv"
   tbl <- hubValidations::read_model_out_file(file_path, hub_path)
-  round_id <- "2022-10-08"
 
   tbl$value[c(1, 10)] <- 150
 
@@ -33,7 +30,6 @@ test_that("check_tbl_value_col_ascending errors correctly", {
 
   hub_path <- system.file("testhubs/flusight", package = "hubUtils")
   file_path <- "hub-ensemble/2023-05-08-hub-ensemble.parquet"
-  round_id <- "2023-05-08"
   tbl <- hubValidations::read_model_out_file(file_path, hub_path)
   tbl_error <- tbl
   tbl_error$target <- "wk ahead inc covid hosp"
