2e1c6a1d321a09e519c9c630a1a267341f8972fb
Anna Krystalli <annakrystalli@googlemail.com>
2023-09-01 12:23:24 +0300

    Add check_valid_round_id_col tests

---
+++ tests/testthat/test-check_valid_round_id_col.R
@@ -0,0 +1,40 @@
+test_that("check_valid_round_id_col works", {
+    hub_path <- system.file("testhubs/simple", package = "hubValidations")
+    file_path <- "team1-goodmodel/2022-10-08-team1-goodmodel.csv"
+    tbl <- arrow::read_csv_arrow(
+        system.file("files/2022-10-15-team1-goodmodel.csv",
+                    package = "hubValidations"
+        ))
+    expect_snapshot(
+        check_valid_round_id_col(
+            tbl = tbl,
+            file_path = file_path,
+            hub_path = hub_path
+        )
+    )
+    expect_snapshot(
+        check_valid_round_id_col(
+            tbl = tbl,
+            file_path = file_path, hub_path = hub_path,
+            round_id_col = "origin_date"
+        )
+    )
+    expect_snapshot(
+        str(check_valid_round_id_col(
+            tbl = tbl,
+            round_id_col = "origin_date",
+            file_path = file_path,
+            hub_path = hub_path
+        ))
+    )
+
+    # Fails with warning when round_id_col invalid
+    expect_snapshot(
+        check_valid_round_id_col(
+            tbl = tbl,
+            file_path = file_path,
+            hub_path = hub_path,
+            round_id_col = "random_column"
+        )
+    )
+})
