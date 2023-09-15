99864c083ae7177d95d9ab6757564dca3b454047
Anna Krystalli <annakrystalli@googlemail.com>
2023-09-15 16:53:41 +0300

    use get_metadata_file_name

--- R/check_submission_metadata_file_exists.R
+++ R/check_submission_metadata_file_exists.R
@@ -4,28 +4,39 @@
 #'
 #' @export
 check_submission_metadata_file_exists <- function(file_path, hub_path = ".") {
-    model_id <- parse_file_name(file_path)$model_id
-    metadata_file_paths <- paste0(model_id, c(".yml", ".yaml"))
-    abs_path <- abs_file_path(metadata_file_paths, hub_path,
-                              subdir = "model-metadata")
-    rel_path <- rel_file_path(metadata_file_paths, hub_path,
-                              subdir = "model-metadata")
-    fe_results <- fs::file_exists(abs_path)
-    check <- any(fe_results)
+  metadata_file_path <- try(
+    get_metadata_file_name(hub_path,
+      file_path,
+      ext = "auto"
+    ), silent = TRUE
+  )
 
-    if (check) {
-      ind <- which(fe_results)
-      msg_attribute <- cli::format_inline("at path {.path {rel_path[ind]}}.")
-    } else {
-      msg_attribute <- cli::format_inline("at path {.path {rel_path[1]}} or
-                                           {.path {rel_path[2]}}.")
-    }
+  check <- !inherits(metadata_file_path, "try-error")
 
-    capture_check_cnd(
-        check = check,
-        file_path = file_path,
-        msg_subject = "Metadata file",
-        msg_attribute = msg_attribute,
-        msg_verbs = c("exists", "does not exist"),
-        error = TRUE)
+  if (check) {
+    rel_path <- rel_file_path(metadata_file_path,
+      hub_path,
+      subdir = "model-metadata"
+    )
+    msg_attribute <- cli::format_inline("at path {.path {rel_path}}.")
+  } else {
+    metadata_file_paths <- get_metadata_file_name(hub_path,
+      file_path,
+      ext = "both"
+    )
+    rel_paths <- rel_file_path(metadata_file_paths, hub_path,
+      subdir = "model-metadata"
+    )
+    msg_attribute <- cli::format_inline("at path {.path {rel_paths[1]}} or
+                                           {.path {rel_paths[2]}}.")
+  }
+
+  capture_check_cnd(
+    check = check,
+    file_path = file_path,
+    msg_subject = "Metadata file",
+    msg_attribute = msg_attribute,
+    msg_verbs = c("exists", "does not exist"),
+    error = TRUE
+  )
 }
