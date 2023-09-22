70ad628321a9d3c340f8eefae907a2fab57ca16b
Anna Krystalli <annakrystalli@googlemail.com>
2023-09-22 18:21:03 +0300

    add try_check file_path arg

--- R/try_check.R
+++ R/try_check.R
@@ -1,12 +1,13 @@
 #' Wrap check expression to capture check execution errors
 #'
 #' @param expr check function expression to run.
+#' @inheritParams check_tbl_colnames
 #'
 #' @return If `expr` executes correctly, the output of `expr` is returned. If
 # ' execution fails, and object of class `<error/check_exec_error>` is returned.
 #'  The execution error message is attached as attribute `msg`.
 #' @export
-try_check <- function(expr) {
+try_check <- function(expr, file_path) {
   check <- try(expr, silent = TRUE)
   if (inherits(check, "try-error")) {
     return(
