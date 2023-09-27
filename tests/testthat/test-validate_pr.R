445f9befbc1b1d29b8a919b6333be8f6cde07755
Anna Krystalli <annakrystalli@googlemail.com>
2023-09-27 12:59:33 +0300

    add validate_pr tests

---
+++ tests/testthat/test-validate_pr.R
@@ -0,0 +1,36 @@
+test_that("validate_pr works on valid PR", {
+
+    temp_hub <- fs::path(tempdir(), "valid_sb_hub")
+    gert::git_clone(url = "https://github.com/Infectious-Disease-Modeling-Hubs/ci-testhub-simple",
+                    path = temp_hub,
+                    branch = "pr-valid")
+
+   checks <- validate_pr(hub_path = temp_hub,
+                gh_repo = "Infectious-Disease-Modeling-Hubs/ci-testhub-simple",
+                pr_number = 4,
+                skip_submit_window_check = TRUE)
+
+   expect_snapshot(str(checks))
+   expect_invisible(check_for_errors(checks))
+
+
+})
+
+test_that("validate_pr works on invalid PR", {
+
+    temp_hub <- fs::path(tempdir(), "invalid_sb_hub")
+    gert::git_clone(url = "https://github.com/Infectious-Disease-Modeling-Hubs/ci-testhub-simple",
+                    path = temp_hub,
+                    branch = "pr-missing-taskid")
+
+    checks <- validate_pr(hub_path = temp_hub,
+                          gh_repo = "Infectious-Disease-Modeling-Hubs/ci-testhub-simple",
+                          pr_number = 5,
+                          skip_submit_window_check = TRUE)
+
+    expect_snapshot(str(checks))
+
+    expect_error(
+        suppressMessages(check_for_errors(checks))
+    )
+})
