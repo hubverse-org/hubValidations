e5857ac70ec1924f0861b08aeb4f4efefd20d778
Anna Krystalli <annakrystalli@googlemail.com>
2023-10-05 10:23:27 +0300

    Export parse_file_name

--- R/parse_file_name.R
+++ R/parse_file_name.R
@@ -1,4 +1,22 @@
+#' Parse model output file metadata from file name
+#'
+#' @param file_path Character string. A model output file name.
+#' Can include parent directories which are ignored.
+#'
+#' @return A list with the following elements:
+#'  - `round_id`: The round ID the model output is associated with.
+#'  - `team_abbr`: The team responsible for the model.
+#'  - `model_abbr`: The name of the model.
+#'  - `model_id`: The unique model ID derived from the concatenation of
+#'  `<team_abbr>-<model_abbr>`.
+#'  - `ext`: The file extension.
+#'
+#' @export
+#'
+#' @examples
+#' parse_file_name("hub-baseline/2022-10-15-hub-baseline.csv")
 parse_file_name <- function(file_path) {
+    checkmate::assert_string(file_path)
     file_name <- tools::file_path_sans_ext(basename(file_path))
 
     split_pattern <- stringr::regex(
