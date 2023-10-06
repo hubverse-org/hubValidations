659a00648c4b09b9539063b8588e4c33fbbe2db6
Anna Krystalli <annakrystalli@googlemail.com>
2023-10-06 12:54:00 +0300

    get opt function directly from namespace. Resolves #51

--- R/exec_cfg_check.R
+++ R/exec_cfg_check.R
@@ -1,17 +1,19 @@
 exec_cfg_check <- function(check_name, validations_cfg, caller_env, caller_call) {
   fn_cfg <- validations_cfg[[check_name]]
   if (!is.null(fn_cfg[["pkg"]])) {
-      fn <- get(fn_cfg[["fn"]],
-                envir = rlang::as_environment(fn_cfg[["pkg"]])
-      )
-  } else if (!is.null(fn_cfg[["source"]])){
-      # TODO Validate source script.
-      source(fn_cfg[["source"]], local = TRUE)
-      fn <- get(fn_cfg[["fn"]])
+    fn <- get(fn_cfg[["fn"]],
+      envir = getNamespace(fn_cfg[["pkg"]])
+    )
+  } else if (!is.null(fn_cfg[["source"]])) {
+    # TODO Validate source script.
+    source(fn_cfg[["source"]], local = TRUE)
+    fn <- get(fn_cfg[["fn"]])
   }
 
   caller_env_formals <- get_caller_env_formals(
-      fn, caller_env, cfg_args = fn_cfg[["args"]])
+    fn, caller_env,
+    cfg_args = fn_cfg[["args"]]
+  )
   args <- c(
     caller_env_formals,
     fn_cfg[["args"]]
@@ -34,7 +36,7 @@ exec_cfg_check <- function(check_name, validations_cfg, caller_env, caller_call)
 get_caller_env_formals <- function(fn, caller_env, cfg_args) {
   caller_env_fmls <- rlang::fn_fmls_names(fn)[
     rlang::fn_fmls_names(fn) %in% rlang::env_names(caller_env) &
-        !rlang::fn_fmls_names(fn) %in% cfg_args
+      !rlang::fn_fmls_names(fn) %in% cfg_args
   ]
- rlang::env_get_list(caller_env, nms = caller_env_fmls, default = NULL)
+  rlang::env_get_list(caller_env, nms = caller_env_fmls, default = NULL)
 }
