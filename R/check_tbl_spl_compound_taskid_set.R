13daf3f26a95a8988b39cbb59852d4c7df33d7b1
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-11 11:03:26 +0300

    Add check_tbl_spl_compound_taskid_set function

---
+++ R/check_tbl_spl_compound_taskid_set.R
@@ -0,0 +1,72 @@
+#' Check model output data tbl sample compound task id sets for each modeling task
+#' match or are coarser than the expected set defined in the config.
+#'
+#' This check detects the compound task ID sets of samples, implied by the `output_type_id`
+#' and task ID values, and checks them for internal consistency and compliacance with
+#' the `compound_taskid_set` defined for each round modeling task in the `tasks.json` config.
+#' @param tbl a tibble/data.frame of the contents of the file being validated.
+#' Column types must **all be character**.
+#' @inherit check_tbl_colnames params
+#' @inherit check_tbl_colnames return
+#' @details If the check fails, the output of the check includes an `errors` element,
+#' a list of items, one for each modeling task failing validation.
+#' The structure depends on the reason the check failed.
+#'
+#' If the check failed because more that a single unique `compound_taskid_set` was found
+#' for a given model task, the `errors` object will be a list with one element for each
+#' `compound_taskid_set` detected and will have the following structure:
+#' - `tbl_comp_tids`: a compound task id set detected in the the tbl.
+#' - `output_type_ids`: The output type ID of the sample that does not contain a
+#' single, unique value for each compound task ID.
+#'
+#' If the check failed because task IDs which is not allowed in the config, were identified
+#' as compound task ID (i.e. samples describe "finer" compound modeling tasks)
+#' for a given model task, the `errors` object will be a list with the structure
+#' described above as well as the additional following elements:
+#' - `config_comp_tids`: the allowed `compound_taskid_set` defined in the modeling
+#' task config.
+#' - `invalid_tbl_comp_tids`: the names of invalid compound task IDs.
+#'
+#' The name of each element is the index identifying the config modeling task the sample is associated with `mt_id`.
+#' See [hubverse documentation on samples](https://hubverse.io/en/latest/user-guide/sample-output-type.html)
+#' for more details.
+#' @export
+check_tbl_spl_compound_taskid_set <- function(tbl, round_id, file_path, hub_path) {
+
+  config_tasks <- hubUtils::read_config(hub_path, "tasks")
+
+  if (isFALSE(has_spls_tbl(tbl)) || isFALSE(hubUtils::is_v3_config(config_tasks))) {
+    return(skip_v3_spl_check(file_path))
+  }
+
+  compound_taskid_set <- get_tbl_compound_taskid_set(tbl, config_tasks, round_id,
+                              compact = TRUE, error = FALSE)
+
+  check <- purrr::map_lgl(compound_taskid_set,
+    ~ is.null(attr(.x, "errors"))
+  ) |> all()
+
+  capture_check_cnd(
+    check = check,
+    file_path = file_path,
+    msg_subject = "All samples in a model task",
+    msg_attribute = "to single, unique compound task ID set that matches or is
+    coarser than the configured {.var compound_taksid_set}.",
+    msg_verbs = c("conform", "do not conform"),
+    details = compile_msg(compound_taskid_set),
+    errors = compile_errors(compound_taskid_set),
+    error = TRUE
+  )
+}
+
+compile_errors <- function(x) {
+  purrr::map(x, ~ attr(.x, "errors")) |>
+    purrr::compact()
+}
+
+compile_msg <- function(x) {
+  purrr::map(x, ~ attr(.x, "msg")) |>
+    purrr::compact() |>
+  purrr::imap_chr(~glue::glue("mt {.y}: {.x}")) |>
+    paste(collapse = ". ")
+}
