a1a8adfff24ce5161346ebf392a7fb5c6fd798f2
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-18 18:02:53 +0300

    Add compound_taskid_set argument to check_tbl_spl_non_compound_tid to allow the override of config compound_taskid_set.

--- tests/testthat/test-check_tbl_spl_non_compound_tid.R
+++ tests/testthat/test-check_tbl_spl_non_compound_tid.R
@@ -36,3 +36,38 @@ test_that("check_tbl_spl_non_compound_tid works", {
     c("check_success", "hub_check", "rlang_message", "message", "condition")
   )
 })
+
+test_that("Overriding compound_taskid_set in check_tbl_spl_compound_tid works", {
+  hub_path <- test_path("testdata/hub-spl")
+  file_path <- "Flusight-baseline/2022-10-22-Flusight-baseline.csv"
+  round_id <- "2022-10-22"
+  config_task <- hubUtils::read_config_file(
+    fs::path(hub_path, "hub-config", "tasks.json")
+  )
+  compound_taskid_set <- list(
+    NULL,
+    c("reference_date", "horizon")
+  )
+  tbl_coarse <- submission_tmpl(
+    config_task = config_task,
+    round_id = round_id,
+    compound_taskid_set = compound_taskid_set
+  ) |>
+    dplyr::filter(.data$output_type == "sample") |>
+    hubData::coerce_to_character()
+
+  # Validation of coarser file with config compound_taskid_set succeeds as the
+  # subset of non compound task IDs remains consistent across samples.
+  expect_snapshot(
+    str(
+      check_tbl_spl_non_compound_tid(tbl_coarse, round_id, file_path, hub_path)
+    )
+  )
+
+  # Validation providing coarser compound taskid set consistent with the data
+  # also succeeds.
+  expect_snapshot(
+    check_tbl_spl_non_compound_tid(tbl_coarse, round_id, file_path, hub_path,
+                               compound_taskid_set = compound_taskid_set)
+  )
+})
