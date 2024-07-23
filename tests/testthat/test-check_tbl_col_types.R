898de794aa8545e66f7a4c3dd56b5787d003a1c4
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-23 16:42:40 +0300

    Correct buggy test

--- tests/testthat/test-check_tbl_col_types.R
+++ tests/testthat/test-check_tbl_col_types.R
@@ -20,15 +20,17 @@ test_that("check_tbl_col_types works", {
   expect_snapshot(
     check_tbl_col_types(tbl, file_path, hub_path)
   )
+})
 
-  # Check "06" location value read and validated correctly
-  hub_path <- test_path("testdata/hub")
-  file_path <- "hub-baseline/2023-04-24-hub-baseline.csv"
-  tbl <- read_model_out_file(
-    file_path,
-    hub_path
-  )
-  expect_snapshot(
-    check_tbl_col_types(tbl, file_path, hub_path)
-  )
+test_that(
+  "Check '06' location value validated correctly in check_tbl_col_types", {
+    hub_path <- test_path("testdata/hub")
+    file_path <- "hub-baseline/2023-04-24-hub-baseline.csv"
+    tbl <- read_model_out_file(
+      file_path,
+      hub_path
+    )
+    expect_snapshot(
+      check_tbl_col_types(tbl, file_path, hub_path)
+    )
 })
