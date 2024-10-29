c44eab92b28d90d3973ca4004939d71499df8abc
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-10-29 14:56:50 -0700

    rewrite helper to allow for multiple checks

--- tests/testthat/helper-custom-fns.R
+++ tests/testthat/helper-custom-fns.R
@@ -16,26 +16,32 @@ stand_up_custom_check_hub <- function(
     # nolint start
     hub_path = system.file("testhubs/flusight", package = "hubValidations"),
     new_path = NULL,
-    check_path = testthat::test_path("testdata/src/R/src_check_works.R")) {
+    check_path = testthat::test_path("testdata/src/R/src_check_works.R"),
+    args = list("src_check_works" = list(extra_msg = 'Extra arguments passed'))
+    ) {
   if (is.null(new_path)) {
     return()
   }
   fs::dir_copy(hub_path, new_path)
   new_path <- fs::path(new_path, fs::path_file(hub_path))
-  withr::with_dir(new_path, create_custom_check("test-check"))
-  fs::file_copy(check_path,
-    fs::path(new_path, "src/validations/R/test-check.R"),
-    overwrite = TRUE
+  script_path <- fs::path(new_path, "src/validations/R/test-check.R")
+  fs::dir_create(fs::path_dir(script_path))
+  fs::file_copy(check_path, script_path, overwrite = TRUE)
+  n <- seq_along(args)
+  fun <- lapply(n, function(fn) {
+      list(
+        fn = names(args)[[fn]],
+        source = fs::path_rel(script_path, start = new_path),
+        args = args[[fn]]
+      )
+  })
+  names(fun) <- paste0("check_", n)
+  yml <- list(
+    "default" = list(
+      "test_custom_checks_caller" = fun
+    )
   )
-  x <- c(
-    'default:',
-    '    test_custom_checks_caller:',
-    '      src_check_works:',
-    '        fn: "src_check_works"',
-    '        source: src/validations/R/test-check.R',
-    '        args:',
-    '          extra_msg: "Extra arguments passed"'
-  )
-  writeLines(x, fs::path(new_path, "hub-config", "validations.yml"))
+  # nolint end
+  writeLines(yaml::as.yaml(yml), fs::path(new_path, "hub-config", "validations.yml"))
   return(new_path)
 }
