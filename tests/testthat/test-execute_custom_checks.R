c2ac758dd8969bf10cc1536df29e6127de78896b
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-10-31 08:24:12 -0700

    simplify tests

--- tests/testthat/test-execute_custom_checks.R
+++ tests/testthat/test-execute_custom_checks.R
@@ -26,15 +26,17 @@ test_that("execute_custom_checks works", {
 
 test_that("execute_custom_checks sourcing functions from scripts works", {
   tmp <- withr::local_tempdir()
-  hub <- stand_up_custom_check_hub(new_path = tmp)
+  the_config <- testthat::test_path("testdata/config/validations-src.yml")
+
+  hub <- stand_up_custom_check_hub(new_path = tmp, yaml_path = the_config)
 
   expect_no_error({
     withr::with_tempdir({
-      res <- test_custom_checks_caller(hub_path = hub)
+      validations_src_external <- test_custom_checks_caller(hub_path = hub)
     })
   })
 
-  expect_snapshot(print(res))
+  expect_snapshot(validations_src_external)
 })
 
 
@@ -42,13 +44,8 @@ test_that("execute_custom_checks return early when appropriate", {
   # When the first custom check returns an check_error class object, custom check
   # execution should return early
   tmp1 <- withr::local_tempdir()
-  the_check <- testthat::test_path("testdata/src/R/src_simple_example_check.R")
   hub1 <- stand_up_custom_check_hub(new_path = tmp1,
-    check_path = the_check,
-    args = list(
-      simple_example_check = list(check = FALSE, error = TRUE),
-      simple_example_check = list(check = TRUE, error = FALSE)
-    )
+    yaml_path = testthat::test_path("testdata/config/validations-early-ret.yml")
   )
   early_ret_custom <- test_custom_checks_caller(hub_path = hub1)
   expect_snapshot(early_ret_custom)
@@ -58,11 +55,7 @@ test_that("execute_custom_checks return early when appropriate", {
   # Same when the first custom check returns an exec_error class object
   tmp2 <- withr::local_tempdir()
   hub2 <- stand_up_custom_check_hub(new_path = tmp2,
-    check_path = the_check,
-    args = list(
-      simple_example_check = list(check = FALSE, error = TRUE, exec_error = TRUE),
-      simple_example_check = list(check = TRUE, error = FALSE)
-    )
+    yaml_path = testthat::test_path("testdata/config/validations-exec-error.yml")
   )
   early_ret_exec_error <- test_custom_checks_caller(hub2)
   expect_snapshot(early_ret_exec_error)
@@ -74,11 +67,7 @@ test_that("execute_custom_checks return early when appropriate", {
   # execution should proceed
   tmp3 <- withr::local_tempdir()
   hub3 <- stand_up_custom_check_hub(new_path = tmp3,
-    check_path = the_check,
-    args = list(
-      simple_example_check = list(check = FALSE, error = FALSE),
-      simple_example_check = list(check = TRUE, error = FALSE)
-    )
+    yaml_path = testthat::test_path("testdata/config/validations-no-early-ret.yml")
   )
   no_early_ret_custom <- test_custom_checks_caller(hub3)
   expect_snapshot(no_early_ret_custom)
