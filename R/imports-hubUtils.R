6080410c38390a8e8c1149ff3cff00f0e6100e55
Anna Krystalli <annakrystalli@googlemail.com>
2024-11-07 16:13:00 +0200

    Import get_hub_* functions moved to hubUtils

---
+++ R/imports-hubUtils.R
@@ -0,0 +1,8 @@
+#' @importFrom hubUtils get_hub_file_formats
+NULL
+
+#' @importFrom hubUtils get_hub_timezone
+NULL
+
+#' @importFrom hubUtils get_hub_model_output_dir
+NULL
--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,3 @@
-get_hub_file_formats <- function(hub_path, round_id) {
-  config_tasks <- hubUtils::read_config(hub_path, "tasks")
-  round_idx <- hubUtils::get_round_idx(config_tasks, round_id)
-  file_formats <- config_tasks[["rounds"]][[round_idx]]$file_format
-  if (!is.null(file_formats)) {
-    return(file_formats)
-  }
-
-  config_admin <- hubUtils::read_config(hub_path, "admin")
-  config_admin$file_format
-}
-
-get_hub_timezone <- function(hub_path) {
-  config_admin <- hubUtils::read_config(hub_path, "admin")
-  config_admin$timezone
-}
-
-get_hub_model_output_dir <- function(hub_path) {
-  config_admin <- hubUtils::read_config(hub_path, "admin")
-  model_output_dir <- config_admin$model_output_dir
-  if (is.null(model_output_dir)) "model-output" else model_output_dir
-}
-
 get_file_target_metadata <- function(hub_path, file_path) {
   round_config <- get_file_round_config(file_path, hub_path)
 
