e5e6f015a16f22ca8ad8c7fd64a57e7259cb0220
Anna Krystalli <annakrystalli@googlemail.com>
2024-11-12 10:06:00 +0200

    Use pipe from dplyr instead of magrittr to silence note about n of imports

--- R/reexport-dplyr.R
+++ R/reexport-dplyr.R
@@ -1,13 +1,10 @@
 #' Pipe operator
 #'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#'
-#' @name %>%
 #' @rdname pipe
 #' @keywords internal
+#' @name  %>%
+#' @importFrom dplyr %>%
 #' @export
-#' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
 #' @return The result of calling `rhs(lhs)`.
