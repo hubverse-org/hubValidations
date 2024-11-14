952f27c31b8898e647d722c21b0d62ae687ad814
Anna Krystalli <annakrystalli@googlemail.com>
2024-11-14 17:07:24 +0200

    Use round instead of rnd

--- R/utils-output_types.R
+++ R/utils-output_types.R
@@ -1,4 +1,4 @@
-get_rnd_required_output_type_names <- function(config_tasks, round_id) {
+get_round_required_output_type_names <- function(config_tasks, round_id) {
   is_required <- get_round_output_types(config_tasks, round_id) |>
     purrr::flatten() |>
     purrr::map_lgl(~ is_required_output_type(.x))
@@ -9,7 +9,7 @@ get_rnd_required_output_type_names <- function(config_tasks, round_id) {
 get_submission_required_output_types <- function(tbl, config_tasks,
                                                  round_id) {
   tbl_output_types <- get_tbl_output_types(tbl)
-  required_output_types <- get_rnd_required_output_type_names(
+  required_output_types <- get_round_required_output_type_names(
     config_tasks,
     round_id
   )
