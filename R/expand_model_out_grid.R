06904c8f690040a2a4db4d2233a9801475bdbc07
Anna Krystalli <annakrystalli@googlemail.com>
2025-01-22 11:47:59 +0200

    Move derived task IDs utility functions to separate file

--- R/expand_model_out_grid.R
+++ R/expand_model_out_grid.R
@@ -465,22 +465,6 @@ null_taskids_to_na <- function(model_task) {
   )
 }
 
-# Set derived task_ids to all NULL values.
-derived_taskids_to_na <- function(model_task, derived_task_ids) {
-  if (!is.null(derived_task_ids)) {
-    purrr::modify_at(
-      model_task,
-      .at = derived_task_ids,
-      .f = ~ list(
-        required = NULL,
-        optional = NA
-      )
-    )
-  } else {
-    model_task
-  }
-}
-
 # Extract the output_type_id values for each model_task object in a round.
 # Input should be the output of subset_round_output_types.
 # config_tid is the name of the output_type_id column in the config schema used
@@ -511,52 +495,4 @@ extract_mt_output_type_ids <- function(x, config_tid, force_output_types = FALSE
       standardise_output_types_ids(output_type_ids, is_required)
     }
   )
-}
-
-
-
-validate_derived_task_ids <- function(derived_task_ids, config_tasks, round_id) {
-  checkmate::assert_character(derived_task_ids, null.ok = TRUE)
-  if (is.null(derived_task_ids)) {
-    return(NULL)
-  }
-  round_task_ids <- hubUtils::get_round_task_id_names(config_tasks, round_id)
-  valid_task_ids <- intersect(derived_task_ids, round_task_ids)
-  if (length(valid_task_ids) < length(derived_task_ids)) {
-    cli::cli_warn(
-      c(
-        "x" = "{.val {setdiff(derived_task_ids, round_task_ids)}}
-        {?is/are} not valid task ID{?s}. Ignored.",
-        "i" = "{.arg derived_task_ids} must be a member of: {.val {round_task_ids}}"
-      ),
-      call = rlang::caller_call()
-    )
-  }
-  model_tasks <- hubUtils::get_round_model_tasks(config_tasks, round_id)
-  has_required <- purrr::map(
-    model_tasks,
-    ~ .x[["task_ids"]][valid_task_ids] %>%
-      purrr::map_lgl(
-        ~ !is.null(.x$required)
-      )
-  ) %>%
-    purrr::reduce(`|`)
-  if (any(has_required)) {
-    cli::cli_abort(
-      c(
-        "x" = "Derived task IDs cannot have required task ID values.",
-        "!" = "{.val {names(has_required)[has_required]}} ha{?s/ve}
-          required task ID values. Ignored."
-      ),
-      call = rlang::caller_call()
-    )
-  }
-  valid_task_ids <- intersect(
-    valid_task_ids,
-    names(has_required)[!has_required]
-  )
-  if (length(valid_task_ids) == 0L) {
-    return(NULL)
-  }
-  valid_task_ids
 }
