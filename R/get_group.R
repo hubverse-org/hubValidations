92234c438d14bfc76141229e95cc9da92935a07a
Anna Krystalli <annakrystalli@googlemail.com>
2025-02-17 16:27:40 +0200

    Add lightweight documentation

--- R/get_group.R
+++ R/get_group.R
@@ -1,18 +1,22 @@
-group_tbl <- function(tbl, mask = NULL, sep = "-") {
+# Group a tbl using values across all columns
+group_tbl <- function(tbl, mask = NULL) {
   if (!is.null(mask)) {
     tbl[mask] <- NA
   }
   dplyr::group_by(tbl, dplyr::pick(dplyr::everything()))
 }
 
-
-get_group_indices <- function(tbl, mask = NULL, sep = "-") {
-  group_tbl(tbl, mask = mask, sep = "-") |>
+# Get a vector of indices of the group each row belongs to when grouping
+# a tbl by all columns
+get_group_indices <- function(tbl, mask = NULL) {
+  group_tbl(tbl, mask = mask) |>
     dplyr::group_indices()
 }
 
-get_group_rows <- function(tbl, mask = NULL, sep = "-") {
-  group_tbl(tbl, mask = mask, sep = "-") |>
+# Get a list of the row indices associated with each group when grouping
+# a tbl by all columns
+get_group_rows <- function(tbl, mask = NULL) {
+  group_tbl(tbl, mask = mask) |>
     dplyr::group_rows()
 }
 
