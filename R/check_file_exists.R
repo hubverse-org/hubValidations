d0b5a0ad432cdeeca6b25b9f1b91cb303d445f31
Anna Krystalli <annakrystalli@googlemail.com>
2025-04-08 17:28:03 +0300

    Add "target-data" to subdir options

--- R/check_file_exists.R
+++ R/check_file_exists.R
@@ -8,7 +8,7 @@
 check_file_exists <- function(file_path, hub_path = ".",
                               subdir = c(
                                 "model-output", "model-metadata",
-                                "hub-config"
+                                "hub-config", "target-data"
                               )) {
   abs_path <- abs_file_path(file_path, hub_path, subdir)
   rel_path <- rel_file_path(file_path, hub_path, subdir) # nolint: object_usage_linter
