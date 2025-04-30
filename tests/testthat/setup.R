73ba319a53da743c0da3b6fcef859a4902124afd
Anna Krystalli <annakrystalli@googlemail.com>
2025-04-30 12:44:32 +0300

    Make setup example temp paths more unique

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,14 +1,23 @@
 # Set up test target data hub
-example_hub_exists <- exists("hub_path") && dir.exists(
-  file.path(hub_path, "target-data")
+example_hub_exists <- exists("complex_forecasting_hub_path") && dir.exists(
+  file.path(complex_forecasting_hub_path, "target-data")
 )
 if (curl::has_internet() && !example_hub_exists) {
   tmp_dir <- withr::local_tempdir(clean = FALSE)
-  hub_path <- fs::path(tmp_dir, "main")
-  file_hub_path <- fs::path(tmp_dir, "file")
-  dir_hub_path <- fs::path(tmp_dir, "dir")
-  example_hub <- "https://github.com/hubverse-org/example-complex-forecast-hub.git"
-  gert::git_clone(url = example_hub, path = hub_path)
-  fs::dir_copy(hub_path, file_hub_path)
-  fs::dir_copy(hub_path, dir_hub_path)
+  example_complex_forecasting_hub_path <- fs::path(tmp_dir, "main")
+  example_file_hub_path <- fs::path(tmp_dir, "file")
+  example_dir_hub_path <- fs::path(tmp_dir, "dir")
+  example_hub_url <- "https://github.com/hubverse-org/example-complex-forecast-hub.git"
+  gert::git_clone(
+    url = example_hub_url,
+    path = example_complex_forecasting_hub_path
+  )
+  fs::dir_copy(
+    example_complex_forecasting_hub_path,
+    example_file_hub_path
+  )
+  fs::dir_copy(
+    example_complex_forecasting_hub_path,
+    example_dir_hub_path
+  )
 }
