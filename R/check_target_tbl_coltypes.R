8ca5346bd9b7d0493fc42695b8f49375409d93e4
Anna Krystalli <annakrystalli@googlemail.com>
2025-07-21 16:30:35 +0300

    Add check_target_tbl_coltypes fn. Resolves #216

--- R/check_target_tbl_coltypes.R
+++ R/check_target_tbl_coltypes.R
@@ -6,26 +6,81 @@
 #' @inherit check_target_tbl_colnames params return
 #' @inheritParams hubData::connect_target_timeseries
 #' @export
-check_target_tbl_coltypes <- function(target_tbl,
-                                      target_type = c(
-                                        "time-series", "oracle-output"
-                                      ),
-                                      date_col = NULL,
-                                      na = c("NA", ""),
-                                      file_path, hub_path) {
+check_target_tbl_coltypes <- function(
+  target_tbl,
+  target_type = c(
+    "time-series",
+    "oracle-output"
+  ),
+  date_col = NULL,
+  na = c("NA", ""),
+  output_type_id_datatype = c(
+    "from_config",
+    "auto",
+    "character",
+    "double",
+    "integer",
+    "logical",
+    "Date"
+  ),
+  file_path,
+  hub_path
+) {
   target_type <- rlang::arg_match(target_type)
+  output_type_id_datatype <- rlang::arg_match(output_type_id_datatype)
 
-  schema <- switch (target_type,
-                    `time-series` = hubData::create_timeseries_schema(
-                      hub_path = hub_path,
-                      date_col = date_col,
-                      na = na
-                    ),
-                    `oracle-output` = hubData::create_oracle_output_schema(
-                      hub_path = hub_path,
-                      na = na
-                    )
+  schema <- switch(
+    target_type,
+    `time-series` = hubData::create_timeseries_schema(
+      hub_path = hub_path,
+      date_col = date_col,
+      na = na,
+      r_schema = TRUE
+    ),
+    `oracle-output` = hubData::create_oracle_output_schema(
+      hub_path = hub_path,
+      na = na,
+      r_schema = TRUE,
+      output_type_id_datatype = output_type_id_datatype
+    )
+  )[colnames(target_tbl)]
+
+  tbl_types <- purrr::map_chr(
+    target_tbl,
+    ~ if (inherits(.x, "numeric")) {
+      typeof(.x)
+    } else {
+      paste(class(.x), collapse = "/")
+    }
   )
+  compare_types <- schema == tbl_types
 
+  check <- all(compare_types)
 
+  if (check) {
+    details <- NULL
+  } else {
+    invalid_cols <- names(compare_types)[!compare_types]
+    details <- paste0(
+      "{.var ",
+      invalid_cols,
+      "} should be {.cls ",
+      schema[invalid_cols],
+      "} not {.cls ",
+      tbl_types[invalid_cols],
+      "}"
+    ) %>%
+      paste(collapse = ", ") %>%
+      paste0(".") %>%
+      cli::format_inline()
+  }
+
+  capture_check_cnd(
+    check = check,
+    file_path = file_path,
+    msg_subject = "Column data types",
+    msg_attribute = cli::format_inline("{target_type} target schema."),
+    msg_verbs = c("match", "do not match"),
+    details = details
+  )
 }
--- tests/testthat/test-check_target_tbl_coltypes.R
+++ tests/testthat/test-check_target_tbl_coltypes.R
@@ -1,18 +1,87 @@
-test_that("check_target_tbl_colnames works on time-series data", {
+test_that("check_target_tbl_coltypes works on csv time-series data", {
+  hub_path <- example_file_hub_path
   target_tbl <- read_target_file("time-series.csv", example_file_hub_path)
   file_path <- "time-series.csv"
 
-  valid_ts <- check_target_tbl_colnames(
+  valid_ts <- check_target_tbl_coltypes(
     target_tbl,
     target_type = "time-series",
     file_path = "time-series.csv",
-    example_file_hub_path
+    hub_path = hub_path
   )
   expect_s3_class(valid_ts, "check_success")
   expect_equal(
     cli::ansi_strip(valid_ts$message) |> stringr::str_squish(),
-    "Column names are consistent with expected column names for time-series target type data."
+    "Column data types match time-series target schema."
   )
 
+  target_tbl$date <- as.character(target_tbl$date)
 
+  invalid_ts <- check_target_tbl_coltypes(
+    target_tbl,
+    target_type = "time-series",
+    file_path = "time-series.csv",
+    hub_path = hub_path
+  )
+  expect_s3_class(invalid_ts, "check_failure")
+  expect_equal(
+    cli::ansi_strip(invalid_ts$message) |> stringr::str_squish(),
+    "Column data types do not match time-series target schema. `date` should be <Date> not <character>." # nolint: line_length_linter
+  )
+})
+
+test_that("check_target_tbl_coltypes works on csv time-series data", {
+  hub_path <- fs::path(tmp_dir, "parquet")
+  fs::dir_copy(example_file_hub_path, hub_path)
+  valid_ts <- read_target_file("time-series.csv", hub_path)
+  arrow::write_parquet(
+    valid_ts,
+    fs::path(hub_path, "target-data", "time-series.parquet")
+  )
+  fs::file_delete(fs::path(hub_path, "target-data", "time-series.csv"))
+
+  target_tbl <- read_target_file(
+    "time-series.parquet",
+    hub_path,
+    coerce_types = "none"
+  )
+  file_path <- "time-series.parquet"
+
+  valid_ts <- check_target_tbl_coltypes(
+    target_tbl,
+    target_type = "time-series",
+    file_path = file_path,
+    hub_path = hub_path
+  )
+  expect_s3_class(valid_ts, "check_success")
+  expect_equal(
+    cli::ansi_strip(valid_ts$message) |> stringr::str_squish(),
+    "Column data types match time-series target schema."
+  )
+
+  invalid_ts <- read_target_file("time-series.parquet", hub_path)
+  invalid_ts$observation <- as.character(target_tbl$observation)
+  arrow::write_parquet(
+    invalid_ts,
+    fs::path(hub_path, "target-data", "time-series.parquet")
+  )
+
+  target_tbl <- read_target_file(
+    "time-series.parquet",
+    hub_path,
+    # To validate parquet data types we read the parquet file without coercing
+    # types to the target schema
+    coerce_types = "none"
+  )
+  invalid_ts <- check_target_tbl_coltypes(
+    target_tbl,
+    target_type = "time-series",
+    file_path = "time-series.csv",
+    hub_path = hub_path
+  )
+  expect_s3_class(invalid_ts, "check_failure")
+  expect_equal(
+    cli::ansi_strip(invalid_ts$message) |> stringr::str_squish(),
+    "Column data types do not match time-series target schema. `observation` should be <double> not <character>." # nolint: line_length_linter
+  )
 })
