110b4096cc62215fef0126211d34fa27c5b5c606
Anna Krystalli <annakrystalli@googlemail.com>
2025-07-29 15:15:12 +0300

    Correct comment

--- tests/testthat/test-check_target_tbl_rows_unique.R
+++ tests/testthat/test-check_target_tbl_rows_unique.R
@@ -122,7 +122,7 @@ test_that("check_target_tbl_rows_unique works oracle-output data", {
   # Check with invalid time-series data. ----
   # -- Non-versioned --
   # Add duplicate rows to the target table.
-  # Change the `observation` column value in one of the
+  # Change the `oracle_value` column value in one of the
   # duplicates to demonstrate it's not being taken into account.
   target_tbl[2:3, ] <- target_tbl[1, ]
   target_tbl[2, "oracle_value"] <- 100 # original was 2380.
