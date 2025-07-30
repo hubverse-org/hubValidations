b324c1044dcf1483dc72ead849d9133a3af1a3e9
Anna Krystalli <annakrystalli@googlemail.com>
2025-07-30 10:40:08 +0300

    Remove unnecessary assignment

--- R/check_file_read.R
+++ R/check_file_read.R
@@ -8,13 +8,13 @@ check_file_read <- function(file_path, hub_path = ".") {
   try_read <- try(
     {
       if (fs::path_ext(file_path) == "csv") {
-        tbl <- read_model_out_file( # nolint: object_usage_linter
+        read_model_out_file(
           file_path = file_path,
           hub_path = hub_path,
           coerce_types = "hub"
         )
       } else {
-        tbl <- read_model_out_file(
+        read_model_out_file(
           file_path = file_path,
           hub_path = hub_path,
           coerce_types = "none"
@@ -29,7 +29,8 @@ check_file_read <- function(file_path, hub_path = ".") {
     details <- NULL
   } else {
     details <- cli::format_inline(
-      attr(try_read, "condition")$message, "\n",
+      attr(try_read, "condition")$message,
+      "\n",
       "Please check file path is correct and file can be read using {.fn read_model_out_file}"
     )
   }
