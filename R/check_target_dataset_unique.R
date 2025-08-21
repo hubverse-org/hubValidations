3b3522d16dbd7c25c9ead0be770b333a4f50c9f6
Anna Krystalli <annakrystalli@googlemail.com>
2025-08-21 12:18:33 +0300

    Return check_error instead of check_failure in check_target_dataset_unique

--- R/check_target_dataset_unique.R
+++ R/check_target_dataset_unique.R
@@ -1,15 +1,17 @@
 #' Check that a single unique target dataset exists for a given target type.
 #'
 #' @inheritParams hubData::get_target_path
-#' @inherit check_tbl_col_types return
+#' @inherit check_target_file_name return
 #' @export
-check_target_dataset_unique <- function(hub_path,
-                                        target_type = c(
-                                          "time-series",
-                                          "oracle-output"
-                                        )) {
+check_target_dataset_unique <- function(
+  hub_path,
+  target_type = c(
+    "time-series",
+    "oracle-output"
+  )
+) {
   target_type <- rlang::arg_match(target_type)
-  file_path <- file.path("target-data", target_type)
+  file_path <- target_type
 
   target_path <- hubData::get_target_path(hub_path, target_type)
   target_n <- length(target_path)
@@ -18,8 +20,10 @@ check_target_dataset_unique <- function(hub_path,
     return(
       capture_check_info(
         file_path = file_path,
-        cli::format_inline("No {.field {target_type}} target type files detected in
-        {.path target-data} directory. Check skipped.")
+        cli::format_inline(
+          "No {.field {target_type}} target type files detected in
+        {.path target-data} directory. Check skipped."
+        )
       )
     )
   }
@@ -31,6 +35,11 @@ check_target_dataset_unique <- function(hub_path,
       "Multiple {.field {target_type}} datasets found:
       {.path {basename(target_path)}}"
     )
+  } else {
+    # If the check passes, return the relative path to the dataset,
+    # otherwise continue returning the target type
+    file_path <- hubData::get_target_path(hub_path, target_type) |>
+      rel_file_path(hub_path, subdir = "target-data")
   }
 
   capture_check_cnd(
@@ -41,7 +50,7 @@ check_target_dataset_unique <- function(hub_path,
       "single unique {.field {target_type}} dataset."
     ),
     msg_verbs = c("contains", "must contain"),
-    error = FALSE,
+    error = TRUE,
     details = details
   )
 }
