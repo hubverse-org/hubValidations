4cd4ee3ac1b7c669c86d434bc71f55833d84d93a
Anna Krystalli <annakrystalli@googlemail.com>
2025-09-03 15:28:11 +0300

    Add validate_target_submission fn. Resolves #263

---
+++ R/validate_target_submission.R
@@ -0,0 +1,109 @@
+#' Validate a submitted target data file.
+#'
+#' Checks both file level properties like
+#' file name, extension, location etc as well as target data, i.e. the
+#' contents of the file.
+#' @inheritParams validate_target_file
+#' @inherit validate_target_data return params
+#' @inheritParams validate_submission
+#' @export
+#' @details
+#'
+#' Details of checks performed by `validate_target_submission()`
+#'
+#' ```{r, echo = FALSE}
+#' arrow::read_csv_arrow(system.file("check_table.csv", package = "hubValidations")) %>%
+#' dplyr::filter(
+#'  .data$`parent fun` %in% c(
+#'                              "validate_target_file",
+#'                              "validate_target_data"
+#'                            ) |
+#'  .data$`check fun` == "check_config_hub_valid",
+#'  !.data$optional
+#'  ) %>%
+#'   dplyr::select(-"parent fun", -"check fun", -"optional") %>%
+#'   dplyr::mutate("Extra info" = dplyr::case_when(
+#'     is.na(.data$`Extra info`) ~ "",
+#'     TRUE ~ .data$`Extra info`
+#'   )) %>%
+#'   knitr::kable() %>%
+#'   kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive")) %>%
+#'   kableExtra::column_spec(1, bold = TRUE)
+#' ```
+#'
+#' @examples
+#' hub_path <- system.file("testhubs/v5/target_file", package = "hubUtils")
+#' validate_target_submission(
+#'   hub_path,
+#'   file_path = "time-series.csv",
+#'   target_type = "time-series"
+#' )
+#' # Example with partitioned data
+#' hub_path <- system.file("testhubs/v5/target_dir", package = "hubUtils")
+#' validate_target_submission(
+#'   hub_path,
+#'   file_path = "time-series/target=wk%20flu%20hosp%20rate/part-0.parquet",
+#'   target_type = "time-series"
+#' )
+validate_target_submission <- function(
+  hub_path,
+  file_path,
+  target_type = c(
+    "time-series",
+    "oracle-output"
+  ),
+  date_col = NULL,
+  round_id = "default",
+  na = c("NA", ""),
+  output_type_id_datatype = c(
+    "from_config",
+    "auto",
+    "character",
+    "double",
+    "integer",
+    "logical",
+    "Date"
+  ),
+  validations_cfg_path = NULL,
+  skip_check_config = FALSE
+) {
+  check_hub_config <- new_target_validations()
+  target_type <- rlang::arg_match(target_type)
+  output_type_id_datatype <- rlang::arg_match(output_type_id_datatype)
+
+  if (!skip_check_config) {
+    check_hub_config$valid_config <- try_check(
+      check_config_hub_valid(hub_path),
+      file_path
+    )
+    if (not_pass(check_hub_config$valid_config)) {
+      return(check_hub_config)
+    }
+  }
+
+  checks_file <- validate_target_file(
+    hub_path = hub_path,
+    file_path = file_path,
+    validations_cfg_path = validations_cfg_path,
+    round_id = round_id
+  )
+
+  if (any(purrr::map_lgl(checks_file, ~ is_any_error(.x)))) {
+    return(
+      combine(check_hub_config, checks_file)
+    )
+  }
+
+  checks_data <- validate_target_data(
+    hub_path = hub_path,
+    file_path = file_path,
+    target_type = target_type,
+    date_col = date_col,
+    na = na,
+    output_type_id_datatype = output_type_id_datatype,
+    validations_cfg_path = validations_cfg_path,
+    round_id = round_id
+  )
+
+  combine(check_hub_config, checks_file, checks_data)
+}
---
+++ tests/testthat/_snaps/validate_target_submission.md
@@ -0,0 +1,62 @@
+# validate_target_submission works on single time-series target data file
+
+    Code
+      res_ts
+    Message
+      
+      -- target_file ----
+      
+      v [valid_config]: All hub config files are valid.
+      
+      -- time-series.csv ----
+      
+      v [target_file_exists]: File exists at path 'target-data/time-series.csv'.
+      i [target_partition_file_name]: Target file path not hive-partitioned. Check skipped.
+      v [target_file_ext]: Target data file extension is valid.
+      v [target_file_read]: target file could be read successfully.
+      v [target_tbl_colnames]: Column names are consistent with expected column names for time-series target type data.
+      v [target_tbl_coltypes]: Column data types match time-series target schema.
+      v [target_tbl_ts_targets]: time-series targets are all valid.
+      v [target_tbl_rows_unique]: time-series target data rows are unique.
+      v [target_tbl_values]: `target_tbl_chr` contains valid values/value combinations.
+      i [target_tbl_output_type_ids]: Check not applicable to time-series target data. Skipped.
+      i [target_tbl_oracle_value]: Check not applicable to time-series target data. Skipped.
+
+# validate_target_submission works on single oracle-output target data file
+
+    Code
+      res_oo
+    Message
+      
+      -- target_file ----
+      
+      v [valid_config]: All hub config files are valid.
+      
+      -- oracle-output.csv ----
+      
+      v [target_file_exists]: File exists at path 'target-data/oracle-output.csv'.
+      i [target_partition_file_name]: Target file path not hive-partitioned. Check skipped.
+      v [target_file_ext]: Target data file extension is valid.
+      v [target_file_read]: target file could be read successfully.
+      v [target_tbl_colnames]: Column names are consistent with expected column names for oracle-output target type data.
+      v [target_tbl_coltypes]: Column data types match oracle-output target schema.
+      i [target_tbl_ts_targets]: Check not applicable to oracle-output target data. Skipped.
+      v [target_tbl_rows_unique]: oracle-output target data rows are unique.
+      v [target_tbl_values]: `target_tbl_chr` contains valid values/value combinations.
+      v [target_tbl_output_type_ids]: oracle-output `target_tbl` contains valid complete output_type_id values.
+      v [target_tbl_oracle_value]: oracle-output `target_tbl` contains valid oracle values.
+
+# validate_target_submission returns early as expected
+
+    Code
+      early_return_ts
+    Message
+      
+      -- target_file ----
+      
+      v [valid_config]: All hub config files are valid.
+      
+      -- time-series/target=wk%20flu%20hosp%20rate/part-0.parquet ----
+      
+      (x) [target_file_exists]: File does not exist at path 'target-data/time-series/target=wk%20flu%20hosp%20rate/part-0.parquet'.
+
